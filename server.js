0984b6c09eecd489735d76eb15e24c466d0b2b46
deboranegrao <deboraafnegrao@gmail.com>
2025-10-26 21:40:30 -0600

    fix database

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const conectaBancoDeDados = require('./bancoDeDados')
 
 const app = express()
 const porta = 3333
+
+// connect to MongoDB
+conectaBancoDeDados();
 
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta", porta)
